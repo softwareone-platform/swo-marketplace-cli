7c83ddfe4f33a8b8bd62e80b19af10173d60eed3
Susana Vázquez <3016283+svazquezco@users.noreply.github.com>
2025-07-09 13:12:17 +0200

    refactor: fix define a constant instead of duplicating the literal

---
+++ swo/mpt/cli/core/accounts/constants.py
@@ -0,0 +1,11 @@
+CREATING = "creating"
+FETCHING = "fetching"
+READING = "reading"
+REMOVING = "removing"
+
+STATUS_MSG = {
+    CREATING: "Making account",
+    FETCHING: "Fetching account information",
+    READING: "Reading accounts from the configuration file",
+    REMOVING: "Removing account",
+}
