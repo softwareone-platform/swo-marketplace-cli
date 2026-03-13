f30eddf187035a727b30add33c28ad51d1c797fb
Susana Vázquez <3016283+svazquezco@users.noreply.github.com>
2026-03-13 15:14:10 +0100

    refactor: Resolve WPS400 WPS407 baseline cleanup

--- cli/core/accounts/constants.py
+++ cli/core/accounts/constants.py
@@ -1,11 +1,13 @@
+from types import MappingProxyType
+
 CREATING = "creating"
 FETCHING = "fetching"
 READING = "reading"
 REMOVING = "removing"
 
-STATUS_MSG = {
+STATUS_MSG = MappingProxyType({
     CREATING: "Making account",
     FETCHING: "Fetching account information",
     READING: "Reading accounts from the configuration file",
     REMOVING: "Removing account",
-}
+})
